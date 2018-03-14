b29ed85c1996e43c83f58d7be7d58692a3e12baf
mtabuteau <mtabuteau@adoria.com>
2018-03-14 14:48:34 +0100

    Module 8

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ToSpacesPipe } from './shared/toSpaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		ProductListComponent,
+		StarComponent,
 		ToSpacesPipe
 	],
 	imports: [
